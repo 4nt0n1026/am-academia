b37313fc82cb3f3fff0b2a45c2ba0b49b83a1157
Christian Storch <christianstorch@iMac21.local>
2013-07-08 11:10:58 -0300

    Modalidae e ParteCorpo atualizadas para o novo modelo de Bean

--- workspace/AMAcademiaJSF/src/br/com/acad/bean/treino/ParteCorpoBean.java
+++ workspace/AMAcademiaJSF/src/br/com/acad/bean/treino/ParteCorpoBean.java
@@ -1,136 +1,73 @@
 package br.com.acad.bean.treino;
 
 import java.io.Serializable;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.acad.bean.Bean;
 import br.com.acad.dao.treino.interf.ParteCorpoDAO;
-import br.com.acad.logic.MessagesLogic;
 import br.com.acad.model.treino.ParteCorpo;
 
 
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class ParteCorpoBean implements Serializable {
+public class ParteCorpoBean extends Bean<ParteCorpo> implements Serializable {
 
 	/************************************************************************************************************/
 	//ATRIBUTOS
 	/************************************************************************************************************/
 	
-	//daos
 	@EJB
 	private ParteCorpoDAO parteCorpoDAO;
 	
-	//ParteCorpo
-	private ParteCorpo parteCorpo;
-	private List<ParteCorpo> partesCorpos;
-	private boolean showParteCorpo;
-
 	/************************************************************************************************************/
 	//METODOS
 	/************************************************************************************************************/
 	
-	/**
-	 * init
-	 */
 	@PostConstruct
-	public void init(){
-		partesCorpos = parteCorpoDAO.buscarTodos();
-		showParteCorpo = false;
+	@Override
+	public void init() {
+		dao = parteCorpoDAO;
+		entities = parteCorpoDAO.buscarTodos();
 	}
 	
 	/**
-	 * mostra painel de inserção de uma nova parteCorpo 
+	 * show form de entity
 	 */
-	public void showNewParteCorpo(){
-		parteCorpo = new ParteCorpo();
-		showParteCorpo = true;
-	}
-	
-	/**
-	 * mostra painel de edição de uma parteCorpo
-	 */
-	public void showEditParteCorpo(){
-		if(parteCorpo==null){
-			MessagesLogic.addWarnMessage("Erro", "Selecione uma parte de corpo para editar");
-		}else{
-			showParteCorpo = true;
-		}
-	}
-	
-	/**
-	 * fecha painel de inserção de parteCorpo
-	 */
-	public void dontShowParteCorpo(){
-		parteCorpo = new ParteCorpo();
-		showParteCorpo = false;
-	}
-	
-	/**
-	 * inclui ou edita parteCorpo no banco
-	 */
-	public void incluirParteCorpo(){
-		if(parteCorpo.getId()==0){
-			parteCorpo = parteCorpoDAO.insert(parteCorpo);
-			partesCorpos.add(parteCorpo);
-		}else{
-			parteCorpoDAO.update(parteCorpo);
-		}
-		parteCorpo = new ParteCorpo();
-		showParteCorpo = false;
-		
-		MessagesLogic.addInfoMessage("Sucesso", "Parte de corpo salva com sucesso");
-	}
-	
-	/**
-	 * deleta parteCorpo do banco
-	 */
-	public void deletarParteCorpo(){
-		if(parteCorpo != null){
-			parteCorpoDAO.removeById(parteCorpo.getId());
-			partesCorpos.remove(parteCorpo);
-			showParteCorpo = false;
-			MessagesLogic.addInfoMessage("Sucesso", "Parte de corpo deletada com sucesso");
-		}else{
-			MessagesLogic.addWarnMessage("Erro", "Selecione uma parte de corpo para deletar");
-		}
-	}
-      
-	/**
-	 * atualiza informações 
-	 */
-	public void atualizar(){
-		partesCorpos = parteCorpoDAO.buscarTodos();
-		parteCorpo = new ParteCorpo();
-		showParteCorpo = false;
-		
-		MessagesLogic.addInfoMessage("Sucesso", "Atualizado");
+	@Override
+	public void showNewEntity() {
+		showEntity = true;
+		entity = new ParteCorpo();
 	}
 
+	/**
+	 * inclui ou edita entity no banco
+	 */
+	@Override
+	public void incluirEntity() {
+		incluirGeneric( entity!=null? entity.getId():0);
+	}
+
+	/**
+	 * deleta entity do banco
+	 */
+	@Override
+	public void deletarEntity() {
+		deletarGeneric(entity!=null?entity.getId():0);
+	}
+	
+	/************************************************************************************************************/
+	//GET FIELDS
+	/************************************************************************************************************/
+	
+	
 
 	/************************************************************************************************************/
 	//GETS E SETS
 	/************************************************************************************************************/
-	public ParteCorpo getParteCorpo() {
-		return parteCorpo;
-	}
-
-	public void setParteCorpo(ParteCorpo parteCorpo) {
-		this.parteCorpo = parteCorpo;
-	}
-
-	public List<ParteCorpo> getPartesCorpos() {
-		return partesCorpos;
-	}
-
-	public boolean getShowParteCorpo() {
-		return showParteCorpo;
-	}
 	
 	
 }
