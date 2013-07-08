6a65b2fcf844069af01fe7609f2e74f36b8abb80
Christian Storch <christianstorch@iMac21.local>
2013-07-08 13:35:04 -0300

    Professor e Funcionario atualizados para o novo modelo Bean (Faltando testar)

--- workspace/AMAcademiaJSF/src/br/com/acad/bean/pessoa/FuncionarioBean.java
+++ workspace/AMAcademiaJSF/src/br/com/acad/bean/pessoa/FuncionarioBean.java
@@ -2,13 +2,12 @@ package br.com.acad.bean.pessoa;
 
 import java.io.Serializable;
 import java.util.Calendar;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.acad.bean.Bean;
 import br.com.acad.dao.pessoa.interf.ProfessorFuncDAO;
 import br.com.acad.logic.MessagesLogic;
 import br.com.acad.model.pessoa.ProfessorFunc;
@@ -17,132 +16,72 @@ import br.com.acad.model.pessoa.ProfessorFunc;
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class FuncionarioBean implements Serializable {
+public class FuncionarioBean extends Bean<ProfessorFunc> implements Serializable {
 
 	/************************************************************************************************************/
 	//ATRIBUTOS
 	/************************************************************************************************************/
 	
-	//daos
 	@EJB
-	private ProfessorFuncDAO funcDAO;
-	
-	//Funcionario
-	private ProfessorFunc funcionario;
-	private List<ProfessorFunc> funcionarios;
-	private boolean showFunc;
-	
-	
+	private ProfessorFuncDAO professorFuncDAO;
 	
 	/************************************************************************************************************/
 	//METODOS
 	/************************************************************************************************************/
 	
-	/**
-	 * init
-	 */
 	@PostConstruct
-	public void init(){
-		funcionarios = funcDAO.buscarTodosFunc();
-		showFunc = false;
+	@Override
+	public void init() {
+		dao = professorFuncDAO;
+		entities = professorFuncDAO.buscarTodos();
 	}
 	
 	/**
-	 * mostra painel de inserção de um novo professorFunc
+	 * show form de entity
 	 */
-	public void showNewFuncionario(){
-		funcionario = new ProfessorFunc();
-		funcionario.setDataNascimento(Calendar.getInstance());
-		showFunc = true;
+	@Override
+	public void showNewEntity() {
+		showEntity = true;
+		entity = new ProfessorFunc();
+		entity.setDataNascimento(Calendar.getInstance());
+	}
+
+	/**
+	 * inclui ou edita entity no banco
+	 */
+	@Override
+	public void incluirEntity() {
+		entity.setIsProfessor(false);
+		incluirGeneric( entity!=null? entity.getId():0);
+	}
+
+	/**
+	 * deleta entity do banco
+	 */
+	@Override
+	public void deletarEntity() {
+		deletarGeneric(entity!=null?entity.getId():0);
 	}
 	
 	/**
-	 * mostra painel de edição de um professorFunc
+	 * Metodo atualizar override para filtrar a busca com somente professores
 	 */
-	public void showEditFuncionario(){
-		if(funcionario==null){
-			MessagesLogic.addWarnMessage("Erro", "Selecione um funcionario para editar");
-		}else{
-			showFunc = true;
-		}
-	}
-	
-	/**
-	 * fecha painel de inserção de professorFunc
-	 */
-	public void dontShowFuncionario(){
-		funcionario = new ProfessorFunc();
-		showFunc = false;
-	}
-	
-	/**
-	 * inclui ou edita professorFunc no banco
-	 */
-	public void incluirFuncionario(){
-		if(funcionario.getId()==0){
-			funcionario.setIsProfessor(false);
-			funcionario = funcDAO.insert(funcionario);
-			funcionarios.add(funcionario);
-		}else{
-			funcDAO.update(funcionario);
-		}
-		funcionario = new ProfessorFunc();
-		showFunc = false;
-		
-		MessagesLogic.addInfoMessage("Sucesso", "Funcionario salvo com sucesso");
-	}
-	
-	/**
-	 * deleta professorFunc do banco
-	 */
-	public void deletarFuncionario(){
-		if(funcionario != null){
-			funcDAO.removeById(funcionario.getId());
-			funcionarios.remove(funcionario);
-			showFunc = false;
-			MessagesLogic.addInfoMessage("Sucesso", "Funcionario deletado com sucesso");
-		}else{
-			MessagesLogic.addWarnMessage("Erro", "Selecione um Funcionario para deletar");
-		}
-	}
-      
-	/**
-	 * atualiza informações 
-	 */
-	public void atualizar(){
-		funcionarios = funcDAO.buscarTodosProf();
-		funcionario = new ProfessorFunc();
-		showFunc = false;
-		
+	@Override
+	public void atualizar() {
+		showEntity = false;
+		entities = professorFuncDAO.buscarTodosFunc();
 		MessagesLogic.addInfoMessage("Sucesso", "Atualizado");
 	}
+	
+	/************************************************************************************************************/
+	//GET FIELDS
+	/************************************************************************************************************/
+	
+	
 
 	/************************************************************************************************************/
 	//GETS E SETS
 	/************************************************************************************************************/
 	
-	public ProfessorFunc getFuncionario() {
-		return funcionario;
-	}
-
-	public void setFuncionario(ProfessorFunc funcionario) {
-		this.funcionario = funcionario;
-	}
-
-	public List<ProfessorFunc> getFuncionarios() {
-		return funcionarios;
-	}
-
-	public void setFuncionarios(List<ProfessorFunc> funcionarios) {
-		this.funcionarios = funcionarios;
-	}
-
-	public boolean getShowFunc() {
-		return showFunc;
-	}
-
-
-	
-
 	
 }
--- workspace/AMAcademiaJSF/src/br/com/acad/bean/pessoa/ProfessorBean.java
+++ workspace/AMAcademiaJSF/src/br/com/acad/bean/pessoa/ProfessorBean.java
@@ -2,13 +2,12 @@ package br.com.acad.bean.pessoa;
 
 import java.io.Serializable;
 import java.util.Calendar;
-import java.util.List;
 
 import javax.annotation.PostConstruct;
-import javax.ejb.EJB;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
+import br.com.acad.bean.Bean;
 import br.com.acad.dao.pessoa.interf.ProfessorFuncDAO;
 import br.com.acad.logic.MessagesLogic;
 import br.com.acad.model.pessoa.ProfessorFunc;
@@ -17,132 +16,72 @@ import br.com.acad.model.pessoa.ProfessorFunc;
 @SuppressWarnings("serial")
 @ManagedBean
 @ViewScoped
-public class ProfessorBean implements Serializable {
+public class ProfessorBean extends Bean<ProfessorFunc> implements Serializable {
 
 	/************************************************************************************************************/
 	//ATRIBUTOS
 	/************************************************************************************************************/
 	
-	//daos
 	@EJB
-	private ProfessorFuncDAO profDAO;
-	
-	//prof
-	private ProfessorFunc prof;
-	private List<ProfessorFunc> professores;
-	private boolean showProf;
-	
-	
+	private ProfessorFuncDAO professorFuncDAO;
 	
 	/************************************************************************************************************/
 	//METODOS
 	/************************************************************************************************************/
 	
-	/**
-	 * init
-	 */
 	@PostConstruct
-	public void init(){
-		professores = profDAO.buscarTodosProf();
-		showProf = false;
+	@Override
+	public void init() {
+		dao = professorFuncDAO;
+		entities = professorFuncDAO.buscarTodos();
 	}
 	
 	/**
-	 * mostra painel de inserção de um novo professor
+	 * show form de entity
 	 */
-	public void showNewProfessorFunc(){
-		prof = new ProfessorFunc();
-		prof.setDataNascimento(Calendar.getInstance());
-		showProf = true;
-	}
-	
-	/**
-	 * mostra painel de edição de um professor
-	 */
-	public void showEditProfessorFunc(){
-		if(prof==null){
-			MessagesLogic.addWarnMessage("Erro", "Selecione um professor para editar");
-		}else{
-			showProf = true;
-		}
-	}
-	
-	/**
-	 * fecha painel de inserção de professorFunc
-	 */
-	public void dontShowProfessorFunc(){
-		prof = new ProfessorFunc();
-		showProf = false;
-	}
-	
-	/**
-	 * inclui ou edita professorFunc no banco
-	 */
-	public void incluirProfessorFunc(){
-		if(prof.getId()==0){
-			prof.setIsProfessor(true);
-			prof = profDAO.insert(prof);
-			professores.add(prof);
-		}else{
-			profDAO.update(prof);
-		}
-		prof = new ProfessorFunc();
-		showProf = false;
-		
-		MessagesLogic.addInfoMessage("Sucesso", "Professor salvo com sucesso");
-	}
-	
-	/**
-	 * deleta professorFunc do banco
-	 */
-	public void deletarProfessorFunc(){
-		if(prof != null){
-			profDAO.removeById(prof.getId());
-			professores.remove(prof);
-			showProf = false;
-			MessagesLogic.addInfoMessage("Sucesso", "Professor deletado com sucesso");
-		}else{
-			MessagesLogic.addWarnMessage("Erro", "Selecione um Professor para deletar");
-		}
-	}
-      
-	/**
-	 * atualiza informações 
-	 */
-	public void atualizar(){
-		professores = profDAO.buscarTodosProf();
-		prof = new ProfessorFunc();
-		showProf = false;
-		
-		MessagesLogic.addInfoMessage("Sucesso", "Atualizado");
+	@Override
+	public void showNewEntity() {
+		showEntity = true;
+		entity = new ProfessorFunc();
+		entity.setDataNascimento(Calendar.getInstance());
 	}
 
-	public ProfessorFunc getProf() {
-		return prof;
+	/**
+	 * inclui ou edita entity no banco
+	 */
+	@Override
+	public void incluirEntity() {
+		entity.setIsProfessor(true);
+		incluirGeneric( entity!=null? entity.getId():0);
 	}
+
+	/**
+	 * deleta entity do banco
+	 */
+	@Override
+	public void deletarEntity() {
+		deletarGeneric(entity!=null?entity.getId():0);
+	}
+	
+	/**
+	 * Metodo atualizar override para filtrar a busca com somente professores
+	 */
+	@Override
+	public void atualizar() {
+		showEntity = false;
+		entities = professorFuncDAO.buscarTodosProf();
+		MessagesLogic.addInfoMessage("Sucesso", "Atualizado");
+	}
+	
+	/************************************************************************************************************/
+	//GET FIELDS
+	/************************************************************************************************************/
+	
+	
 
 	/************************************************************************************************************/
 	//GETS E SETS
 	/************************************************************************************************************/
-
-	public void setProf(ProfessorFunc prof) {
-		this.prof = prof;
-	}
-
-	public List<ProfessorFunc> getProfessores() {
-		return professores;
-	}
-
-	public void setProfessores(List<ProfessorFunc> professores) {
-		this.professores = professores;
-	}
-
-	public boolean getShowProf() {
-		return showProf;
-	}
-
-
-	
 	
 	
 }
